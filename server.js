408eb9e51c18d8ec8ea3fee3432d400af1bfd775
Thembisile  Makamu <makamuthe2@gmail.com>
2024-07-17 06:55:35 +0200

    updated

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const http = require("http");
 const socketIo = require("socket.io");
-
+const path = require("path");
 const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
 
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, 'public')));
 
 io.on("connection", (socket) => {
   console.log("a user connected");
